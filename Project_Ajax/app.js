dee2345e190e663ae6ec763f834691f86f00e389
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-04-09 19:15:25 -0400

    html update image to display and fit the screen

--- Project_Ajax/app.js
+++ Project_Ajax/app.js
@@ -6,10 +6,10 @@ $(()=>{
 
 
   $.ajax({
-
+      url: 'https://api.thecatapi.com/v1/images/search'
   }).then(
       (data)=>{
-          
+        console.log(data)
       },
       ()=>{
           console.log('bad request');
