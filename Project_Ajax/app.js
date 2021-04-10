50304ba000db06e24ff6a6fbf2f07fd4a33083ef
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-04-10 01:17:53 -0400

    update more functional site

--- Project_Ajax/app.js
+++ Project_Ajax/app.js
@@ -1,5 +1,5 @@
 
-
+const quotes = [`All our dreams can come true, if we have the courage to pursue them. – Walt Disney`, `The secret of getting ahead is getting started. – Mark Twain`, `I’ve missed more than 9,000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life and that is why I succeed. – Michael Jordan`, `Everything you can imagine is real.― Pablo Picasso`, `Happiness is not something ready made. It comes from your own actions. ― Dalai Lama XIV`, `Don’t be pushed around by the fears in your mind. Be led by the dreams in your heart. – Roy T. Bennett`, `I never lose. Either I win or learn. – Nelson Mandela`, `Nothing can dim the light that shines from within. – Maya Angelou`, `There may be people that have more talent than you, but there’s no excuse for anyone to work harder than you. – Derek Jeter`]
 
 $(()=>{
 
